ee64c2e16966130d6b0045015af20e59581340f4
isharathilina <isharawap@.com>
2020-02-06 18:25:46 +0530

    debug 1

--- final/final.ino
+++ final/final.ino
@@ -1,6 +1,18 @@
 
 #include <WiFi.h>
 #include "PubSubClient.h"
+#include <SPI.h>
+#include <MFRC522.h>
+#include <HX711.h>
+#include "TinyGPS++.h"
+#include <EEPROM.h>
+
+#define EEPROM_SIZE 1
+#define ageDataAddr 0
+
+#define buzerOutPin 34
+
+#define DEBUG
  
 const char* ssid = "Cloud"; // Enter your WiFi name
 const char* password =  "11115555"; // Enter WiFi password
@@ -12,52 +24,39 @@ const char* mqttPassword = "je1T5MADj3Wi";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-
-//------------------
-#include <SPI.h>
-#include <MFRC522.h>
-
-// ifid circuit wiring spi bus + 
+// rfid pins
 #define SS_PIN 12
-#define RST_PIN 13 //22
+#define RST_PIN 13 
  
 MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 MFRC522::MIFARE_Key key; 
 // Init array that will store new NUID 
 byte nuidPICC[4];
 
-//--------------------
-
-#include <HX711.h>
-
-// HX711 circuit wiring
+// load cell pins
 const int LOADCELL_DOUT_PIN = 21; //4
 const int LOADCELL_SCK_PIN = 22;
 float calibration_factor = 2125; //-7050 
 
 HX711 scale;
-//----------------
-
-#include "TinyGPS++.h"
 TinyGPSPlus gps;
 
+// serial 2 connect to gps
 
 
 void setup() 
 { 
   Serial.begin(9600);
   Serial2.begin(9600); // for gps data 
+  EEPROM.begin(EEPROM_SIZE);
 
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522 
   
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);  // load cell start
   
-  
-  
-  
-  
   WiFi.begin(ssid, password);
+  pinMode(buzerOutPin, OUTPUT); // buzzer output
  
   if (WiFi.status() != WL_CONNECTED) 
   {
@@ -96,21 +95,46 @@ void loop()
   client.loop(); // wait incoming data
   tryToConnect(); // try to connect server
   
+  
+  Serial.println(GetRfidUuid()); //getLoadCellData str
+  Serial.println(getLoadCellData()); //getLoadCellData lng
+  Serial.println(getGpsData()); // str
+
+  checkBagWeight(getLoadCellData());
+  
+  char rfidDataBuf[30];
+  GetRfidUuid().toCharArray(rfidDataBuf, 30);
+
+  client.publish("send", rfidDataBuf);
+  
+  Sendsms("0000000000", getGpsData());
 
 
-//Serial.println(GetRfidUuid()); //getLoadCellData
-//Serial.println(getLoadCellData()); //getLoadCellData
-Serial.println(getGpsData());
-
-  client.publish("send", "System started");
-
-
-
-delay(1000);
+  delay(1000);
   
 }
 
 
+
+void checkBagWeight(long BagWeightRow)
+{
+	  int childAge = EEPROM.read(ageDataAddr);
+	  Serial.println(childAge);
+	  
+	  int bagWeight = BagWeightRow/5000;
+	  if(bagWeight>childAge*(3/10))
+	  {
+		digitalWrite(buzerOutPin, HIGH);
+		delay(500);
+		digitalWrite(buzerOutPin, LOW);
+		delay(500);
+
+	  }else
+	  {
+		digitalWrite(buzerOutPin, LOW);
+	  }
+
+  }
 
 
 void tryToConnect()
@@ -139,25 +163,70 @@ void tryToConnect()
   }
 
 
-
 }
  
+
+  
 void callback(char* topic, byte* payload, unsigned int length)
 {
- 
+
+  //client.publish("flavaSMS", "data received..!");
+   
+  #ifdef DEBUG
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
+  #endif
+  
+  char* dataArry = (char*)malloc(length);
  
+  #ifdef DEBUG
   Serial.print("Message:");
-  for (int i = 0; i < length; i++) 
+  #endif
+  for (int i = 0; i < length; i++)
   {
-    Serial.print((char)payload[i]);
+    dataArry[i] = (char)payload[i];
+
   }
- 
-  Serial.println();
-  Serial.println("-----------------------");
- 
+
+  #ifdef DEBUG
+  Serial.print("length ");
+  Serial.println(length);
+  #endif
+
+  Serial.println(dataArry);
+  
+   // start converting
+  String *strData;
+  strData = new String[length];
+
+  int count =0;
+  char * pch;
+  pch = strtok (dataArry,"=");
+  
+  while (pch != NULL)
+  {
+    String tempst= pch;
+    strData[count] = tempst;
+    #ifdef DEBUG
+    printf ("%s\n",pch);
+    #endif
+    pch = strtok (NULL, "=");
+    count++;
+  }
+  
+  Serial.print("Name :- ");
+  Serial.println(strData[0]);
+  Serial.print("Age data :- ");
+  Serial.println(strData[1]);
+  //Sendsms(strData[0],strData[1]);
+  
+  EEPROM.write(ageDataAddr, strData[1].toInt());
+  EEPROM.commit();
+
+ free(dataArry);
 }
+ 
+
 
 
 String getGpsData()
@@ -165,24 +234,35 @@ String getGpsData()
   
  if(Serial2.available() > 0)
  {
-    gps.encode(Serial2.read());
-    if (gps.location.isUpdated())
+	for(int i=0; i<10; i++)
+	{
+	
+		gps.encode(Serial2.read());
+		if (gps.location.isUpdated())
+		{
+		  Serial.print("Latitude= "); 
+		  Serial.print(gps.location.lat(), 6);
+		  Serial.print(" Longitude= "); 
+		  Serial.println(gps.location.lng(), 6);
+		  
+		  String gpsData;
+		  gpsData+= String(gps.location.lat());
+		  gpsData+= ",";
+		  gpsData+= String(gps.location.lng());
+
+		
+		return gpsData;
+		break;
+		}else
+		{
+			Serial.println("Initialize GPS");
+		}
+	
+	}
+   
+  }else
   {
-      Serial.print("Latitude= "); 
-      Serial.print(gps.location.lat(), 6);
-      Serial.print(" Longitude= "); 
-      Serial.println(gps.location.lng(), 6);
-    
-    return "data";
-    }else{
-    Serial.println("Initialize GPS");
-
-  
-  }
-  }else{
     Serial.println("No serial data");
-
-  
   }
 
 
@@ -296,4 +376,41 @@ void printDec(byte *buffer, byte bufferSize)
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], DEC);
   }
+}
+
+// AT+CMGR=1\r    // read 1st sms
+// AT+CNMI=1,2,0,0,0  // enable not
+
+int Sendsms(String num, String msg){
+
+  Serial2.print("AT\r\n");
+  delay(500);
+  while(!Serial2.available());  //ready gsm
+  
+  while(Serial2.available())
+  {   //garbadge collection
+    //Serial.print((char)Serial2.read());
+    int temp = (char)Serial2.read();
+  }
+    
+  Serial2.print("AT+CMGF=1\r\n");   // Text Mode
+  delay(100);
+  
+  Serial2.print("AT+CMGS=\"");
+  Serial2.print(num);
+  Serial2.print("\"\r\n");
+  delay(100);
+  
+  Serial2.println(msg);
+  delay(50);
+  Serial2.println((char)26);    //CTRL+Z  for end msg
+  delay(50);
+    
+  Serial.print("Msg sent to ");
+  Serial.print(num);
+  Serial.print(" \r\n");
+
+
+  return 1;
+  
 }
